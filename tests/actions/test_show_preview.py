1fc69d125a3ca957b63cb3b32162fbb636d44ec0
Bernardo Martorano <bernardormartorano@gmail.com>
2023-07-05 18:39:25 -0300

    req03 first test

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,8 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+
+
+def test_show_preview_no_context(capsys):
+    context = {"all_files": [], "all_dirs": []}
+    show_preview(context)
+    message = capsys.readouterr()
+    assert message.out == 'Found 0 files and 0 directories\n'
