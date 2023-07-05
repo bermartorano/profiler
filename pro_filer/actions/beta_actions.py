2d59a945ff2394b55789a8c561de2383cabed288
Bernardo Martorano <bernardormartorano@gmail.com>
2023-07-05 15:39:45 -0300

    req01

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -5,7 +5,10 @@ def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(
+            context["all_files"],
+            key=lambda file_path: file_path.count('/')
+        )
         print(f"Deepest file: {deepest_file}")
 
 
